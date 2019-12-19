554a34b97527240d3c336284284cc26312b261f3
foramtrada232 <foramtrada232@gmail.com>
2019-12-19 19:12:50 +0530

    responsive changes

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,16 @@
 $(document).ready(function () {
     $('.header_slider').slick({
-        // dots: trule,
         infinite: true,
-        // speed: 300,
         slidesToShow: 1,
         arrows: true,
         prevArrow: '<button class="slick-prev" aria-label="Previous" type="button">Previous</button>',
         nextArrow: '<button class="slick-next" aria-label="Next" type="button">Next</button>',
         autoplay: true,
         autoplaySpeed: 3000,
-        // adaptiveHeight: true,
     });
     
     $('.latest_news_slider').slick({
         dots: true,
-        // infinite: true,
-        // speed: 300,
         slidesToShow: 3,
         arrows: true,
         prevArrow: false,
